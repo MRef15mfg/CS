feedbfb473fa34285287abc3a5ea25c0c41a4407
MRef15mfg <mrefmfg@gmail.com>
2026-05-22 12:09:20 +0200

    IntroToNet

---
+++ IntroToNet/Program.cs
@@ -0,0 +1,11 @@
+﻿// using System;
+namespace MyNamespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\"It's easy to win forgiveness for being wrong;\nbeing right is what gets you into real trouble.\"\n\t\t\t\t\\Bjarne Stroustrup\\");
+        }
+    }
+}
